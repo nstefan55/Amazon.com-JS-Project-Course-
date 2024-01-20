73c22ca37af0d31c7faae5863c62f2b5ede2b819
Nikola Stefancic <nikola.stefancic@gmail.com>
2024-01-20 15:08:39 +0100

    Save Cart in Local Storage

--- data/cart.js
+++ data/cart.js
@@ -1,15 +1,24 @@
 // exporting cart variable so it can be used in other js files
 
-export let cart = [
-  {
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2,
-  },
-  {
-    productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-    quantity: 1,
-  },
-];
+export let cart = JSON.parse(localStorage.getItem("cart")); //converting string to array
+
+//if there is no cart in localStorage, cart will be given default values
+if (!cart) {
+  cart = [
+    {
+      productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+      quantity: 2,
+    },
+    {
+      productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+      quantity: 1,
+    },
+  ];
+}
+
+export function saveToStorage() {
+  localStorage.setItem("cart", JSON.stringify(cart)); //converting the cart into string and saving to local storage
+}
 
 export function addToCart(productId) {
   let matchingItem; //takes productId and adds it to the cart
@@ -51,6 +60,8 @@ export function addToCart(productId) {
       quantity,
     });
   }
+
+  saveToStorage(); //updating the local storage
 }
 
 export function removeFromCart(productId) {
@@ -65,4 +76,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart; //replacing the old cart with the updated cart
+
+  saveToStorage();
 }
