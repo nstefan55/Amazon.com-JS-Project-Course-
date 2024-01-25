ed52e7e60266c282c65207d2066212ec4d4344c5
Nikola Stefancic <nikola.stefancic@gmail.com>
2024-01-25 10:57:33 +0100

    Rename tests-jasmine folder into tests

---
+++ tests-jasmine/tests-without-framework/test-money.js
@@ -0,0 +1,33 @@
+import { formatCurrency } from '../../jscripts/utils/money.js';
+
+// Examples of Test Cases
+
+console.log('TEST SUITE: formatCurrency');
+
+console.log('converts cents into dollars');
+if (formatCurrency(2095) === '20.95') {
+  console.log('PASSED');
+} else {
+  console.log('FAILED!');
+}
+
+console.log('works with number 0');
+if (formatCurrency(0) === '0.00') {
+  console.log('PASSED');
+} else {
+  console.log('FAILED!');
+}
+
+console.log('rounds to the nearest cent');
+if (formatCurrency(2000.5) === '20.01') {
+  console.log('PASSED');
+} else {
+  console.log('FAILED!');
+}
+
+console.log('rounds cents to smaller number');
+if (formatCurrency(2000.4) === '20.00') {
+  console.log('PASSED');
+} else {
+  console.log('FAILED!');
+}
