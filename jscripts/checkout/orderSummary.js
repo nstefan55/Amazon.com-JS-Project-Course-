e34c5b8c895ba9d10f6ad9119a015edb92a487f2
Nikola Stefancic <nikola.stefancic@gmail.com>
2024-01-25 10:49:11 +0100

    Create automated tests for orderSummary

--- jscripts/checkout/orderSummary.js
+++ jscripts/checkout/orderSummary.js
@@ -36,7 +36,7 @@ export function renderOrderSummary() {
     cartSummaryHTML += `
       <div class="cart-item-container js-cart-item-container-${
         matchingProduct.id
-      }">
+      } js-cart-item-container">
         <div class="delivery-date">Delivery date: ${dateString}</div>
 
         <div class="cart-item-details-grid">
@@ -52,7 +52,9 @@ export function renderOrderSummary() {
             <div class="product-price">${formatCurrency(
               matchingProduct.priceCents
             )}</div> 
-            <div class="product-quantity">
+            <div class="product-quantity js-product-quantity-${
+              matchingProduct.id
+            }">
               <span> Quantity: <span class="quantity-label js-quantity-label-${
                 matchingProduct.id
               }">${cartItem.quantity}</span> </span>
@@ -67,9 +69,9 @@ export function renderOrderSummary() {
               <span class="save-quantity-link link-primary js-save-link" data-product-id="${
                 matchingProduct.id
               }">Save</span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${
+              <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${
                 matchingProduct.id
-              }">
+              }" data-product-id="${matchingProduct.id}">
                 Delete
               </span>
             </div>
